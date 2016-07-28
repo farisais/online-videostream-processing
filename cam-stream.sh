d982eb66f9e107d9a78777579e0c6916f15c2757
Faris Rahman <farisais@faris-mac.local>
2016-07-28 16:53:19 +0700

    add linux command to capture cam from ffmpeg

--- cam-stream.sh
+++ cam-stream.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-ffmpeg -f qtkit -i "0" -vcodec libx264 -x264-params keyint=8 -f h264 -pix_fmt yuv420p -r 30 -vf scale=640:480 http://localhost:8080/vstream?secret=1234
+UNAME=$(uname -s)
+if [ "$UNAME" == "Darwin" ]; then
+	ffmpeg -f qtkit -i "0" -vcodec libx264 -x264-params keyint=8 -f h264 -pix_fmt yuv420p -r 30 -vf scale=640:480 http://localhost:8080/vstream?secret=1234
+else
+	if [ "$UNAME" == "Linux" ]; then
+		ffmpeg -f -i /dev/video0 -vcodec libx264 -x264-params keyint=8 -f h264 -pix_fmt yuv420p -r 30 -vf scale=640:480 http://localhost:8080/vstream?secret=1234
+	fi
+fi
